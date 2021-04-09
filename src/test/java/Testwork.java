9af88a2d64d6c8ff0fdf5632c7ed5090d2ce470e
Win10Pro <npavlenkov@gmail.com>
2021-04-09 17:19:31 +0300

    new add

--- src/test/java/Testwork.java
+++ src/test/java/Testwork.java
@@ -4,21 +4,26 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class Testwork {
     @Test
-
-    void Form () {
+    void Form() {
         open("https://demoqa.com/automation-practice-form");
         $x("//input[@placeholder='First Name']").setValue("Stas");
         $x("//input[@placeholder='Last Name']").setValue("Stasov");
         $x("//input[@placeholder='name@example.com']").setValue("Stasov@input.su");
         $x("//label[@for='gender-radio-2']").click();
         $x("//input[@id='userNumber']").setValue("9874520123");
-        $x("//div[@id='dateOfBirth]").click();
+        $x("//input[@id='dateOfBirthInput']").click();
+        $x("//select[@class='react-datepicker__month-select']").click();
+        $x("//option[@value='4']").click();
+        $x("//select[@class='react-datepicker__year-select']").click();
+        $x("//option[@value='1988']").click();
+        //$x("//select[@class='react-datepicker__year-select']").click();
+        $x("//div[@id='subjectsContainer']").setValue("Ar");
+
 
 
         sleep(5000);
     }
 
 
-
-    }
+}
 
