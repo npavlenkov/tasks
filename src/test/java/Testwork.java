1883dd6efeb67e2147823a95a2325a6b26f43da1
Win10Pro <npavlenkov@gmail.com>
2021-04-12 10:49:52 +0300

    new add6

--- src/test/java/Testwork.java
+++ src/test/java/Testwork.java
@@ -1,3 +1,4 @@
+
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -6,15 +7,23 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class Testwork {
     @BeforeAll
-    static void setup(){
+    static void setup() {
         Configuration.startMaximized = true;
     }
+
     @Test
     void Form() {
+        String
+                firstName ="Stas",
+                lastName = "Stasov",
+                userEmail = "Stasov@input.su";
+
+
+
         open("https://demoqa.com/automation-practice-form");
-        $x("//input[@placeholder='First Name']").setValue("Stas");
-        $x("//input[@placeholder='Last Name']").setValue("Stasov");
-        $x("//input[@placeholder='name@example.com']").setValue("Stasov@input.su");
+        $x("//input[@id='firstName']").setValue(firstName);
+        $x("//input[@id='lastName']").setValue(lastName);
+        $x("//input[@id='userEmail']").setValue(userEmail);
         $x("//label[@for='gender-radio-2']").click();
         $x("//input[@id='userNumber']").setValue("9874520123");
         $x("//input[@id='dateOfBirthInput']").click();
@@ -35,7 +44,18 @@ public class Testwork {
         $x("//div[@class=' css-1wa3eu0-placeholder']").click();
         $x("//*[text()='Delhi']").click();
         $x("//button[@id='submit']").click();
-
+        //$x("//div[@id='example-modal-sizes-title-lg']]").shouldHave(text("Thanks for submitting the form"));
+        //Проверка формы
+        //$x("//td[text()='Student Name')']").parent().shouldHave(text()firstName + " " + lastName");
+        //$x("//td[text()='Student Email')']").parent().shouldHave(text()"Stasov@input.su");
+        //$x("//td[text()='Gender')']").parent().shouldHave(text()"Stasov@input.su");
+        //$x("//td[text()='Mobile')']").parent().shouldHave(text()"Stasov@input.su");
+        //$x("//td[text()='Date of Birth')']").parent().shouldHave(text()"Stasov@input.su");
+        //$x("//td[text()='Subjects')']").parent().shouldHave(text()"Stasov@input.su");
+        //$x("//td[text()='Hobbies')']").parent().shouldHave(text()"Stasov@input.su");
+        //$x("//td[text()='Picture')']").parent().shouldHave(text()"Stasov@input.su");
+        //$x("//td[text()='Address')']").parent().shouldHave(text()"Stasov@input.su");
+        //$x("//td[text()='State and City')']").parent().shouldHave(text()"Stasov@input.su");
 
 
 
