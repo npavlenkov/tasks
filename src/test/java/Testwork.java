dfe39f8550f57262c9c6d0a17a24465180367fd3
Win10Pro <npavlenkov@gmail.com>
2021-04-13 13:02:09 +0300

    work2 done v1.1

--- src/test/java/Testwork.java
+++ src/test/java/Testwork.java
@@ -1,5 +1,6 @@
 
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -10,21 +11,27 @@ public class Testwork {
     @BeforeAll
     static void setup() {
         Configuration.startMaximized = true;
+
     }
+
+    String
+            firstName = "Stas",
+            lastName = "Stasov",
+            userEmail = "Stasov@input.su",
+            userNumber = "9874520123",
+            picture = "banner.png",
+            state = "NCR",
+            city = "Delhi",
+            hobby3 = "Music",
+            Gender = "Female";
+
 
     @Test
     void Form() {
-        String
-                firstName = "Stas",
-                lastName = "Stasov",
-                userEmail = "Stasov@input.su",
-                userNumber = "9874520123";
-
-
 
 
         open("https://demoqa.com/automation-practice-form");
-        $x("//input[@id='firstName']").setValue(firstName);
+        $x("//input[@id='firstName']").val(firstName);
         $x("//input[@id='lastName']").setValue(lastName);
         $x("//input[@id='userEmail']").setValue(userEmail);
         $x("//label[@for='gender-radio-2']").click();
@@ -39,27 +46,27 @@ public class Testwork {
         $x("//*[text()='Arts']").click();
         $x("//input[@id='subjectsInput']").setValue("E");
         $x("//*[text()='English']").click();
-        $x("//label[@for='hobbies-checkbox-3']").click();
-        $x("//input[@id='uploadPicture']").sendKeys("C://banner.png");
+        $x("//div[@id='hobbiesWrapper']//label[text()='"+hobby3+"']").click();
+        $x("//input[@id='uploadPicture']").uploadFromClasspath("img/" + picture);
         $x("//textarea[@id='currentAddress']").setValue("shemisheika, stepnaya street");
-        $x("//div[@class=' css-1wa3eu0-placeholder']").click();
-        $x("//*[text()='NCR']").click();
-        $x("//div[@class=' css-1wa3eu0-placeholder']").click();
-        $x("//*[text()='Delhi']").click();
+        $x("//div[@id='state']").click();
+        $x("//div[contains(@id,'react-select')][text()='" + state + "']").click();
+        $x("//div[@id='city']").click();
+        $x("//div[contains(@id,'react-select')][text()='" + city + "']").click();
         $x("//button[@id='submit']").click();
         $x("//div[@id='example-modal-sizes-title-lg']").shouldHave(text("Thanks for submitting the form"));
         //Проверка формы
-        $x("//td[text()='Student Name']").parent().shouldHave(text(firstName + " " + lastName));
+        Assertions.assertEquals($x("//td[text()='Student Name']/parent::tr/td[2]").getText(), firstName + " " + lastName, "не найдено искомое");
         $x("//td[text()='Student Email']").parent().shouldHave(text(userEmail));
-        $x("//td[text()='Gender']").parent().shouldHave(text("Female"));
+//        $x("//td[text()='Gender']").parent().shouldHave(text("Female"));
+        Assertions.assertEquals($x("//td[text()='Gender']/parent::tr/td[2]").getText(), Gender);
         $x("//td[text()='Mobile']").parent().shouldHave(text(userNumber));
         $x("//td[text()='Date of Birth']").parent().shouldHave(text("11 May,1988"));
         $x("//td[text()='Subjects']").parent().shouldHave(text("Arts, English"));
-        $x("//td[text()='Hobbies']").parent().shouldHave(text("Music"));
+        $x("//td[text()='Hobbies']").parent().shouldHave(text(hobby3));
         $x("//td[text()='Picture']").parent().shouldHave(text("banner"));
         $x("//td[text()='Address']").parent().shouldHave(text("shemisheika, stepnaya street"));
         $x("//td[text()='State and City']").parent().shouldHave(text("NCR Delhi"));
-
 
 
         sleep(5000);
